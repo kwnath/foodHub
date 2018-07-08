863376d130bc85ae0178a2b529c1a1f4ae2a7cfa
nathan <kwnath@gmail.com>
2018-07-08 10:56:57 +0800

    test

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="page">
-    <div class="view-wrapper">
+<div>
+    <div v-if="!started">
+      <img @click="getStarted" :src="homepage" class="home" alt="">
+    </div>
+    <div v-if="!view">
+      <img :src="receipt" class="home" alt="" @click="clickReceipt()">
+      <input accept="image/*" capture="camera" ref="testingImage" type="file" id="image" style="display:none" @change="processFile($event)"/>
+    </div>
+    <div class="page">
+    <div v-if="view">
+      <div class="view-wrapper">
       <div v-if="selectedTab()==0">
         <calendar/>
       </div>
@@ -13,11 +22,12 @@
         <Product/>
       </div>
       <div v-if="selectedTab()==4">
-        <div>Market Place</div>
       </div>
+    </div>
     </div>
     <tabbar class="tabbar"></tabbar>
   </div>
+</div>
 </template>
 
 <script>
@@ -30,6 +40,10 @@ import Product from 'components/Product'
 export default {
   data() {
     return {
+      started: false,
+      view: false,
+      homepage: require("../assets/home.png"),
+      receipt: require("../assets/receipt.png")
     }
   },
   components: {
@@ -43,6 +57,24 @@ export default {
   computed: {
   },
   methods: {
+    clickReceipt(){
+         this.view = true
+         this.started=true
+         this.$refs.testingImage.click()
+         return
+    },
+    processFile(event) {
+      console.log("hello")
+      this.$store.state.selectedTab = 1
+      this.$store.state.loading = true
+      let that = this
+    },
+    getStarted() {
+      this.started = true
+    },
+    showView() {
+      return this.view = true
+    },
     selectedTab() {
       let test = this.$store.state.selectedTab
       return test
@@ -55,7 +87,13 @@ export default {
 }
 </script>
 <style>
-
+.home {
+  width: 100vw;
+  height: 100vh;
+  z-index: 10000;
+  position: relative;
+  background-color: white;
+}
 .page {
   height: 100vh;
   width: 100vw;
